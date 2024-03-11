c0dd2c49fcc7544f0abc4c48173c2d316baa9bca
Naina0702 <nandriamihoatra@gmail.com>
2024-03-11 13:42:19 +0300

    60

--- src/app/contenu/hira/hira.component.ts
+++ src/app/contenu/hira/hira.component.ts
@@ -21,52 +21,62 @@ export class HiraComponent implements OnInit{
     {
       "titre":"Aoreno Mafy",
       "rythme":"karitaky",
-      "image":"Album_1/aoreno.png"
+      "image":"Album_1/aoreno.png",
+      "Lien_youtube":"https://www.youtube.com/watch?v=vxOnS2MFbpQ"
     },
     {
       "titre":"Ety ny vavahady",
       "rythme":"Sud.Af",
-      "image":"Album_1/ety_.png"
+      "image":"Album_1/ety_.png",
+      "Lien_youtube":""
     },
     {
       "titre":"Fahasoavanao",
       "rythme":"",
-      "image":"Album_1/fahasoavanao.png"
+      "image":"Album_1/fahasoavanao.png",
+      "Lien_youtube":""
     },
     {
       "titre":"Faneva AMIKRI",
       "rythme":"",
-      "image":"Album_1/faneva.png"
+      "image":"Album_1/faneva.png",
+      "Lien_youtube":""
     },
     {
       "titre":"Fantantrao",
       "rythme":"slow ballad",
-      "image":"Album_1/fantatrao.png"
+      "image":"Album_1/fantatrao.png",
+      "Lien_youtube":""
     },
     {
       "titre":"Jesosy hiady ho anao",
       "rythme":"Classique",
-      "image":"Album_1/jesosy_hiady.png"
+      "image":"Album_1/jesosy_hiady.png",
+      "Lien_youtube":""
     },
     {
       "titre":"Lehibe ny fitiavany",
       "rythme":"Country",
-      "image":"Album_1/lehibe_ny fitiavany.png"
+      "image":"Album_1/lehibe_ny fitiavany.png",
+      "Lien_youtube":""
     },    
     {
       "titre":"Mifona",
       "rythme":"Slow",
-      "image":"Album_1/mifona.png"
+      "image":"Album_1/mifona.png",
+      "Lien_youtube":""
     },
     {
       "titre":"Ny Aminao",
       "rythme":"Afro.beat",
-      "image":"Album_1/ny aminao.png"
+      "image":"Album_1/ny aminao.png",
+      "Lien_youtube":""
     },
     {
       "titre":"Rongatry",
       "rythme":"Batrelaky",
-      "image":"Album_1/rongatry.png"
+      "image":"Album_1/rongatry.png",
+      "Lien_youtube":""
     },
   ]
 
@@ -74,52 +84,62 @@ export class HiraComponent implements OnInit{
     {
       "titre":"Hakanto",
       "rythme":"Slow",
-      "image":"Album_2/Hakanto.png"
+      "image":"Album_2/Hakanto.png",
+      "Lien_youtube":""
     },
     {
       "titre":"Hanaraka anao",
       "rythme":"Ba gasy",
-      "image":"Album_2/Hanaraka anao.png"
+      "image":"Album_2/Hanaraka anao.png",
+      "Lien_youtube":""
     },
     {
       "titre":"Jesosy safidinay",
       "rythme":"Gospel",
-      "image":"Album_2/Jesosy safidinay.png"
+      "image":"Album_2/Jesosy safidinay.png",
+      "Lien_youtube":""
     },
     {
       "titre":"K'aboda etsana Jesosy",
       "rythme":"Batrelaky",
-      "image":"Album_2/Kaboda.png"
+      "image":"Album_2/Kaboda.png",
+      "Lien_youtube":""
     },
     {
       "titre":"Mamafaza voa tsara",
       "rythme":"slow",
-      "image":"Album_2/Mamafaza voa.png"
+      "image":"Album_2/Mamafaza voa.png",
+      "Lien_youtube":""
     },
     {
       "titre":"Ny tena fifaliako",
       "rythme":"Country",
-      "image":"Album_2/Nytena fifaliako.png"
+      "image":"Album_2/Nytena fifaliako.png",
+      "Lien_youtube":""
     },
     {
       "titre":"Olom-baovao",
       "rythme":"",
-      "image":"Album_2/Olom-baovao.png"
+      "image":"Album_2/Olom-baovao.png",
+      "Lien_youtube":""
     },    
     {
       "titre":"O ry velona ô",
       "rythme":"Ba gasy",
-      "image":"Album_2/O ry velona o.png"
+      "image":"Album_2/O ry velona o.png",
+      "Lien_youtube":""
     },
     {
       "titre":"Ray mpahary",
       "rythme":"Classique",
-      "image":"Album_2/Ray mpahary.png"
+      "image":"Album_2/Ray mpahary.png",
+      "Lien_youtube":""
     },
     {
       "titre":"Zao riano",
       "rythme":"Karitaky",
-      "image":"Album_2/zao riano.png"
+      "image":"Album_2/zao riano.png",
+      "Lien_youtube":""
     },
   ]
   private unsubscribe$ = new Subject<void>();
