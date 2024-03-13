e9ab23bdfb1937e533b2623f07b837ed795b97b7
Naina0702 <nandriamihoatra@gmail.com>
2024-03-13 12:23:28 +0300

    ps

--- dist/frontend/server/main.server.mjs
+++ dist/frontend/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-JRWHQ6SR.mjs";import"./chunk-ULC627HY.mjs";import"./chunk-6PBJT3S7.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-WLAQJT4I.mjs";import"./chunk-ULC627HY.mjs";import"./chunk-6PBJT3S7.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
