cb4c53aa8100a5ef2b4676e404d2f879a455d833
Naina0702 <nandriamihoatra@gmail.com>
2024-03-14 07:08:28 +0300

    text-responsive

--- dist/frontend/server/main.server.mjs
+++ dist/frontend/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-HXWULAFU.mjs";import"./chunk-ULC627HY.mjs";import"./chunk-6PBJT3S7.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-W3B3M3VF.mjs";import"./chunk-ULC627HY.mjs";import"./chunk-6PBJT3S7.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
